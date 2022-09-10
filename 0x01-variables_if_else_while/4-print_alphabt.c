085892fb8d96f497214cec706f5e17f34cafdf50
nanjala24 <nanjala77.cs@gmail.com>
2022-09-10 10:42:09 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,19 +3,20 @@
 #include <time.h>
 /**
  * main - Entry point
- * Author:nanjala24
+ * Author: nanjala24
  * Return: 0
  */
 int main(void)
 {
 	char alphabet;
 
-	for (alphabet= 'a' ; alphabet <= 'z' ; alphabet++)
+	for (alphabet = 'a' ; alphabet <= 'z' ; alphabet++)
 	{
 		if (alphabet != 'e' && alphabet != 'q')
 		{
 			putchar(alphabet);
 		}
-	}putchar('\n');
+	}
+	putchar('\n');
 	return (0);
 }
