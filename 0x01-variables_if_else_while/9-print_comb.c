a59fa2c6081a3ecbece321f51be0e6460e996493
nanjala24 <nanjala77.cs@gmail.com>
2022-09-10 12:12:12 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -entry point
+ * Description: a program that prints all possible combinations of single-digit numbers.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int numberz;
+	
+	for (numberz = 48; numberz < 58; numberrz++)
+	{
+		putchar(numberz);
+		if (numberz != 57)
+				{
+				putchar(',');
+				putchar(' ');
+				}
+				}
+				putchar('\n');
+				return (0)
+
+				}
