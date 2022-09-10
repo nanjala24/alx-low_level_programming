2ddbfeddcedba4d1a2ddf245af669ad8991ff984
nanjala24 <nanjala77.cs@gmail.com>
2022-09-10 09:55:43 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *
+ * main - Entry point
+ *
+ * Description: using the main function
+ * this projects prints "programming is positive, zero or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 30; numbers < 40 numbers++)
+		putchar(numbers);
+	putchar('\n');
+	return (0);
+}
