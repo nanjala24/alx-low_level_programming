f84401ad9a6569c9e6b16bf8760dc65bedde20cd
nanjala24 <nanjala77.cs@gmail.com>
2022-09-10 11:35:22 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/. gcc 7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time>
+/**
+ * main -entry point
+ * Author: nanjala24
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+	for (c = 'z'; c >= 'a'; c--)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+
+}
