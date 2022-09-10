bdd7dcd98be24314ffeb2eb2307a12dcce9fe3f5
nanjala24 <nanjala77.cs@gmail.com>
2022-09-10 09:39:43 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: using the main function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	for (i = 0; i < 10; i++)
+	{
+		printf("%i", i);
+	}
+	printf("\n");
+	return (0)
+}
