41d9927c0e109baae64d2766e0c0a07acbfaee8e
nanjala24 <nanjala77.cs@gmail.com>
2022-09-09 22:16:04 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Author: nanjala24
+ * Details: c program to print positive,zero and negative results of a random number
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int n;
+	srand(time(0));
+	n=rand() - RAND-MAX / 2;
+
+	if (n >0)
+		printf("%d is positive\n", n);
+	else if (n ==0)
+		printf("%d is zero\n", n);
+	esle
+		printf("%d is negative\n", n);
+
+	return (0);
+}
