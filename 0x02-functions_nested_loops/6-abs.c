1b8053190bca4f742389028b2dca48123c385db3
nanjala24 <nanjala77.cs@gmail.com>
2022-09-15 14:16:50 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs - function that computes the absolute integer
+ * 
+ * Return: 0 on success
+ */
+int _abs(int c)
+{
+	if (c > o || c == 0)
+	{
+		return (c);
+	}
+	else
+	{
+		return (c * -1);
+	}
+}
