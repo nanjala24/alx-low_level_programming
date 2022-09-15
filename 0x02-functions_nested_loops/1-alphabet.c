759ee96355c4588ea224b55dbd520f6abcc56fe1
nanjala24 <nanjala77.cs@gmail.com>
2022-09-15 10:35:01 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,19 +2,16 @@
 
 /**
  * print_alphabet - function to print abc
- * Return: 0
+ *
  *
  */
 
 void print_alphabet(void)
 {
-	char c;
-	
+	int c;
 	for (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(c);
-	}
 	_putchar(c);
-}
+	}
 _putchar('\n');
 }
