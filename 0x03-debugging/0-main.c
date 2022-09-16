d6d84f400711b715f9c124b7997e0540742aa115
nanjala24 <nanjala77.cs@gmail.com>
2022-09-16 10:47:55 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - function to print if integer
+ * positive or negative
+ *
+ * task 0
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	positive_or_negative(i);
+
+	return (0);
+
+}
