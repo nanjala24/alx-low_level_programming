587d707177fb39eb8d436eac5b7294cc71ae1064
nanjala24 <nanjala77.cs@gmail.com>
2022-09-16 15:58:53 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,21 +2,15 @@
 
 /**
  * main - function to print if integer is positive or negative
- * positive or negative
- *
- * task 0
- *
  * Return: 0
  */
 
 int main(void)
 {
-	int i;
+int i;
 
-	i = 0;
+i = 0;
+positive_or_negative(i);
 
-	positive_or_negative(i);
-
-	return (0);
-
+return (0);
 }
