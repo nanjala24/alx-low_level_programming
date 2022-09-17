6fcba159e06d20e96280c0e5b2b56cea0ef7a14c
nanjala24 <nanjala77.cs@gmail.com>
2022-09-16 17:48:05 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * more_numbers - prints numbers to 14
+ * more_numbers - print numbers to 14
  *
  * Return: returns nothing
  */
@@ -13,8 +13,10 @@ int n1, n2;
 
 for (n1 = 0; n1 < 10; n1++)
 {
-for (n2 = 0; n2 <= 14; n2**)
+
+for (n2 = 0; n2 <= 14 ; n2++)
 {
+
 if (n2 > 9)
 {
 putchar((n2 / 10) + '0');
@@ -23,4 +25,4 @@ putchar((n2 % 10) + '0');
 }
 putchar(10);
 }
-}
+{
