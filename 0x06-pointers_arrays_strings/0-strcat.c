dc332f4f870d88e5232b7028f0617c2d4f212d1c
nanjala24 <nanjala77.cs@gmail.com>
2022-09-23 22:50:56 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,20 +9,14 @@
 
 char *_strcat(char *dest, char *src)
 {
-int count = 0, count2 = 0;
+int index = 0, dest_len = 0;
 
-while (*(dest + count) != '\0')
-{
-	count++;
-}
+while (dest[index++])
+	dest_len++;
 
-while (count2 >= 0)
-{
-	*(dest + count) = *(src + count2);
-	if (*(src + count2) == '\0')
-	break;
-	count++;
-	count2++;
-}
+for (index = 0; src[index]; index++)
+	dest[dest_len++] = src[index];
+
 return (dest);
+
 }
