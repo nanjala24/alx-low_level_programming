7cee8f0e4a44557282829f229d5935661e2e2c8d
nanjala24 <nanjala77.cs@gmail.com>
2022-10-13 14:18:21 -0700

    5-free_dog.c

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "main_h"
+#include <stdlib.h>
+
+/**
+ * free_dog - frees dogs.
+ * @d: The dog to be freed.
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
