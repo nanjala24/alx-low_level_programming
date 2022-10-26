7c07e38337ed5287a9802bf16a3ecb08af72e50b
nanjala24 <nanjala77.cs@gmail.com>
2022-10-26 01:59:37 -0700

    2-add_node.c

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -7,7 +7,7 @@
 
 /**
  * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
+ * @n: integer
  * @len: length of the string
  * @next: points to the next node
  *
@@ -16,17 +16,19 @@
 
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
+	int n;
 	struct list_s *next;
-}list_t;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
+void free_list2(list_t **head);
 int _strlen(const char *str);
+int pop_list(list_t **head);
+list_t *get_nodeint_at_index(list_t *head, unsigned int index);
 
 #endif	/*LISTS_H*/
 
